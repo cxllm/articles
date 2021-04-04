dbe9e302e61352b3c14d5a1bc2a3811febc50ea6
Callum <cxllm@outlook.fr>
2021-04-04 10:35:59 +0100

    Updated nvm article

--- articles/04-04-21-nvm.py
+++ articles/04-04-21-nvm.py
@@ -3,6 +3,6 @@ content = {
     "title": "Using NVM (Node Version Manager) to install Node.js",
     "description": "A guide on how to install Node Version Manager on Linux/MacOS/WSL systems and using it for convenient node.js installations and version switching.",
     "published": "Sunday, 04 Apr 2021",
-    "updated": None,
+    "updated": "Sunday, 04 Apr 2021",
     "endpoint": "nvm-for-nodejs",
 }
