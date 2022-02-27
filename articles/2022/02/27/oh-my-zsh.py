0d3cd604ba95ffae0fbf4d668b0a14be80a693c3
Callum <hello@cxllm.com>
2022-02-27 20:59:40 +0000

    Oh My ZSH article

--- articles/2022/02/27/oh-my-zsh.py
+++ articles/2022/02/27/oh-my-zsh.py
@@ -1,7 +1,1 @@
-content = {
-    "md": "oh-my-zsh.md",
-    "title": "Installing Oh My ZSH",
-    "description": "A guide on how to install Oh My ZSH on Linux/MacOS/WSL",
-    "published": "Sunday, 27 February 2022",
-    "updated": None,
-}
+content={'md': 'oh-my-zsh.md', 'title': 'Installing Oh My ZSH', 'description': 'A guide on how to install Oh My ZSH on Linux/MacOS/WSL', 'published': 'Sunday, 27 February 2022', 'updated': 'Sunday, 27 February 2022'}
